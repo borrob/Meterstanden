fe61051fc4966c0d9d93da43ca36da61b41dc765
Rob van Loon <borrob@me.com>
2016-12-31 21:54:09 +0100

    Remove console.log statement

--- src/main/javascript/app/meterstanden.service.ts
+++ src/main/javascript/app/meterstanden.service.ts
@@ -22,7 +22,6 @@ export class MeterstandenService {
 	}
 	
 	private handleError(error: any): Promise<any> {
-		console.error('An error occurred', error); // for demo purposes only
 		return Promise.reject(error.message || error);
 	}
 }
